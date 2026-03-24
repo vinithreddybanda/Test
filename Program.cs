b6482c6765bea0e85c2a1af997c22eafcefbb1fd
Vinith Reddy Banda <2480146@cognizant.com>
2026-03-24 15:03:04 +0530

    Hello World

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+        }
+    }
+}
