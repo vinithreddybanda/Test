5377948ae532a9b80afe4c0abe2ff77622b9e271
Vinith Reddy Banda <2480146@cognizant.com>
2026-03-24 22:34:39 +0530

    okie okie

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿sbyte a=1;
+short b =2;
+ushort c=3;
+int d=10;
+uint e=15;
+long l=1444444444444444444;
+ulong ul= 9992999;
+float f = 7777.773333333333333337f;
+double dble= 888888.8888888888888888888d;
+System.Console.WriteLine(a);
+System.Console.WriteLine(b);
+System.Console.WriteLine(c);
+System.Console.WriteLine(d);
+System.Console.WriteLine(e);
+System.Console.WriteLine(l);
+System.Console.WriteLine(ul);
+System.Console.WriteLine(f);
+System.Console.WriteLine(dble);
+
+
+
