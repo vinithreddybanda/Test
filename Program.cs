2c470c1a396dfdc299abd73a94e1601bedd435a7
Vinith Reddy Banda <2480146@cognizant.com>
2026-03-24 15:16:00 +0530

    CosnoleClass

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Drawing;
+using System.Security.AccessControl;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hi, This is Console Class");
+        System.Console.ReadKey(); //reads a character
+        System.Console.ReadLine();
+        System.Console.Clear(); //Clear Screen
+        System.Console.WriteLine("Hello Again");
+        Console.Read();
+        Console.WriteLine("Bye");
+
+    }
+}
