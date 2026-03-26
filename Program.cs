737ba44a476683cbda7a02170294d53b08683a41
vinithreddybandacts <vinithreddybandacts@github.com>
2026-03-27 00:26:05 +0530

    Literals

--- Program.cs
+++ Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        
+        System.Console.WriteLine("Literals:  The fixed values - Not Changed ");
+        System.Console.WriteLine();
+        System.Console.WriteLine(" values of types int, uint, long, ulong, etc");
+        System.Console.WriteLine();
+
+        System.Console.WriteLine("C#, Octal Number Representation is not possible");
+
+        System.Console.WriteLine();
+
+        System.Console.WriteLine("");
+
+        System.Console.WriteLine(" int x=101"); //no suffix-prefix
+        int x1 =101; 
+        
+
+        System.Console.WriteLine("int x = 0X123F"); //0x prefix and f is suffix
+        int x2 = 0X123F; //base-16
+
+        System.Console.WriteLine("int x = 0b1111");//0b prefix
+        int x3 = 0b1111;  // binary 1-0
+
+
+        System.Console.WriteLine();
+        System.Console.WriteLine();
+
+        System.Console.WriteLine("base :"+ x1);
+        System.Console.WriteLine("HEx :"+ x2);
+        System.Console.WriteLine("Binary: "+ x3);
+
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Literals
+
+![](https://dotnettutorials.net/wp-content/uploads/2022/07/word-image-27425-1-2.png)
+
+# output
+
+![alt text](image.png)
