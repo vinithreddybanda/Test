090d1278fbeaf12fa4d0bd8e301f8e7332a42773
vinithreddybandacts <vinithreddybandacts@github.com>
2026-03-26 23:10:55 +0530

    initial TTYL

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        
+        System.Console.WriteLine("Reference Type\n - store the reference of a variable");
+
+        System.Console.WriteLine("Predefined - Objects , Strings , dynamics");
+        System.Console.WriteLine("User defined - classes and Interfaces");
+
+        
+    }
+}
