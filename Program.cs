73b6639d9fe4580b401b617675d490a465529a7b
vinithreddybandacts <vinithreddybandacts@github.com>
2026-03-27 00:38:50 +0530

    StringLiterals begginers

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int a =10;
+        System.Console.WriteLine("\a"); // escape 
+        System.Console.WriteLine(@"\a");
+        System.Console.WriteLine($"{a}");
+        System.Console.WriteLine(@"{a}"); //verbatim string literal
+        System.Console.WriteLine("\u0041"); //unicode 
+    }
+}
